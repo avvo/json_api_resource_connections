a75d695f87373b57c336fd1c23eaee6e00f91d15
Brian <broberts@avvo.com>
2017-08-15 11:35:47 -0700

    update test expectations

--- test/server_not_ready_error_test.rb
+++ test/server_not_ready_error_test.rb
@@ -4,6 +4,7 @@ class ServerNotReadyErrorTest < Minitest::Test
 
   def test_message_adds_previous_error
     error = JsonApiResource::Connections::ServerNotReadyError.new('some message')
-    assert_equal '', error.message
+    expected = 'JsonApiResource::Connections::ServerNotReadyError  *** Previous Error:: [some message]'
+    assert_equal expected, error.message
   end
 end
